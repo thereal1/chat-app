d3a18a700cf9b8ce62399c6c96cb59b4d14e4214
Zac Workman <zac.workman45@gmail.com>
2017-04-10 17:09:57 +0100

    Added form user interface and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,5 +9,17 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New message received', message);
+  const li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
